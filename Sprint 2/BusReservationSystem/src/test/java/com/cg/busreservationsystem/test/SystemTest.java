6b17b61a2e56637ea0e839eed42e733961d0f608
agm221b <adityamishr123@gmail.com>
2019-09-17 11:06:03 +0530

    testing underway

--- Sprint 2/BusReservationSystem/src/test/java/com/cg/busreservationsystem/test/SystemTest.java
+++ Sprint 2/BusReservationSystem/src/test/java/com/cg/busreservationsystem/test/SystemTest.java
@@ -6,21 +6,45 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class SystemTest {
+import com.cg.busreservationsystem.dao.UserDao;
+import com.cg.busreservationsystem.dao.UserDaoImpl;
+import com.cg.busreservationsystem.dto.Bus;
 
+class SystemTest {
+	UserDao userDao;
+	Bus bus1;
+	Bus bus2;
+	
 	@BeforeEach
 	void beforeEachTest() {
+		userDao = new UserDaoImpl();
+		
+		bus1 = null;
+		bus2 = new Bus();
+		bus2.setBusClass("ac");
+		bus2.setBusType("sleeper");
+		bus2.setBusName("volvo");
+		bus2.setSource("mumbai");
+		bus2.setDestination("pune");
+		//List<E> 
+		//bus2.setDayOfJourney(dayOfJourney);
+		
 		
 	}
 	
+	
+	
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		
+		
+		
 	}
 	
 	@AfterEach
 	void afterEachTest() {
-		
+		userDao =null;
 	}
 
 }
